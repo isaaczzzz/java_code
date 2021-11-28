1152a813cd9301cb2223816bce0d0425ca8efb1e
Isaac Zhang <isaac-z@outlook.com>
2021-11-28 15:00:34 +0800

    update

--- Learn/src/ArrayClass.java
+++ Learn/src/ArrayClass.java
@@ -5,8 +5,16 @@ public class ArrayClass {
         int[] a = new int[] { 18, 62, 68, 82, 65, 9 };
         int[] b = Arrays.copyOfRange(a, 0, 5); //复制a到b
         //<==>System.arraycopy(a, 0, b, 0, 5 );
-        String content = Arrays.toString(a); //转换为String
+
         Arrays.sort(a); //排序
         System.out.println("排序后: " + Arrays.toString(a));
+        //String content = Arrays.toString(a);
+        // 转换为String
+
+        System.out.println("数字62出现的位置:"+Arrays.binarySearch(a, 62));//必须先排序
+
+        int[] c = new int[5];
+        Arrays.fill(a, 1);
+        System.out.println("c: " + Arrays.toString(a));
     }
 }
